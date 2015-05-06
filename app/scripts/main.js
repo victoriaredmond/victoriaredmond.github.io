aa327997f7858ca17413ebcd29a070565e37220b
Andrew Beniston <andrew.beniston@gmail.com>
2015-05-06 16:06:58 +0100

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+'use strict';
+var owl = $('.owl-primary');
+
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    mouseDrag: false,
+    dots: true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true
+}).on('click', function(){
+    $(this).find('.owl-next').trigger('click');
+});
+
+$('.portfolio-options button').on('click', function(){
+    var parent = $(this).parents('.portfolio');
+
+    if( $('.owl-primary', parent).hasClass('active')){
+        $('.portfolio-info', parent).toggleClass('active');
+        $('.owl-primary', parent).toggleClass('active');
+    }else{
+        $('.owl-primary', parent).toggleClass('active');
+        $('.portfolio-info', parent).toggleClass('active');
+    }
+    
+});
+
